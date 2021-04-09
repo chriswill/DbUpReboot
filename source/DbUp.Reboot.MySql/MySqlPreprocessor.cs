4c09b2624e5f659bc798f93c8e1f348ac72c0ad2
Chris Williams <chris.williams@gaine.com>
2021-04-09 14:05:02 -0700

    Support for the MySql database

---
+++ source/DbUp.Reboot.MySql/MySqlPreprocessor.cs
@@ -0,0 +1,15 @@
+﻿using DbUp.Reboot.Engine;
+
+namespace DbUp.Reboot.MySql
+{
+    /// <summary>
+    /// This preprocessor makes adjustments to your sql to make it compatible with MySql.
+    /// </summary>
+    public class MySqlPreprocessor : IScriptPreprocessor
+    {
+        /// <summary>
+        /// Performs some preprocessing step on a MySql script.
+        /// </summary>
+        public string Process(string contents) => contents;
+    }
+}
